676516f1f97792d12249b246549b61798670431b
Sakda45 <sakda.yok@ku.th>
2024-09-10 21:49:20 +0700

    1009

--- load/load-index.js
+++ load/load-index.js
@@ -1,16 +1,12 @@
-// ใช้ JavaScript โหลดไฟล์ navbar.html
+// load-index.js
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../page/index.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('page-index').innerHTML = data;
-        })
-        window.loadPage = function(page) {
-            fetch(page)
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('page-index').innerHTML = data;
-                })
-                .catch(error => console.error('Error loading page:', error));
-        };
+    // ฟังก์ชันสำหรับโหลดเนื้อหาใหม่
+    window.loadPage = function(page) {
+        fetch(page)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('page-index').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading page:', error));
+    };
 });
